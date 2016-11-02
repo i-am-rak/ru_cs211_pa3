8164dc778c19bd861c9158502499b0883355ac5b
raknahsa <raknahsa@gmail.com>
2016-11-02 14:39:03 -0400

    Modify nCr.s
    Add 32 bit assembly code instead of 64 bit.

--- formula/nCr.s
+++ formula/nCr.s
@@ -1,65 +1,51 @@
 .globl nCr
         .type   nCr, @function
 nCr:
-  # Your code for nCr should go here
-	
-    pushq	%r13
-	pushq	%r12
-	pushq	%rbp
-	pushq	%rbx
-	movl	%edi, %ebp
-	movl	%esi, %r12d
+  # Your code for nCr should go here	
+	pushl	%ebp
+	pushl	%edi
+	pushl	%esi
+	pushl	%ebx
+	movl	20(%esp), %esi
+	movl	24(%esp), %ebp
+	pushl	%esi
 	call	Factorial
 	movl	%eax, %ebx
-	movl	%r12d, %edi
+	movl	%ebp, (%esp)
 	call	Factorial
-	movl	%eax, %r13d
-	movl	%ebp, %edi
-	subl	%r12d, %edi
+	movl	%eax, %edi
+	subl	%ebp, %esi
+	movl	%esi, (%esp)
 	call	Factorial
-	movl	%r13d, %ecx
+	addl	$4, %esp
+	movl	%edi, %ecx
 	imull	%eax, %ecx
 	movl	%ebx, %eax
 	cltd
 	idivl	%ecx
-	popq	%rbx
-	popq	%rbp
-	popq	%r12
-	popq	%r13
+	popl	%ebx
+	popl	%esi
+	popl	%edi
+	popl	%ebp
 	ret
-
-    
-   # movl	%edi, %ebp
-#	call	Factorial
-#	movl	%eax, %ebx
-#	movl	%esi, %edi
-#	call	Factorial
-#	movl	%ebp, %edi
-#	subl	%esi, %edi
-#	call	Factorial
-#	movl	%eax, %ecx
-#	imull	%eax, %ecx
-#	movl	%ebx, %eax
-#	cltd
-#	idivl	%ecx
-#	ret
 
 .globl Factorial
         .type   Factorial, @function
 Factorial:
   # Your code for Factorial should go here
-    testl	%edi, %edi
+	movl	4(%esp), %ecx
+	testl	%ecx, %ecx
 	jle	.L4
-	addl	$1, %edi
+	addl	$1, %ecx
 	movl	$1, %eax
 	movl	$1, %edx
 .L3:
 	imull	%edx, %eax
 	addl	$1, %edx
-	cmpl	%edi, %edx
+	cmpl	%ecx, %edx
 	jne	.L3
-	ret
+	rep ret
 .L4:
 	movl	$1, %eax
 	ret
-	
+
