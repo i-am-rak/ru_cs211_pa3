e93af98b148dd33c23e65920b4e954e2d17b7909
raknahsa <raknahsa@gmail.com>
2016-11-03 21:03:42 -0400

    Remove negative testing
    Remove code to check if negative number.

--- formula/formula.c
+++ formula/formula.c
@@ -5,6 +5,13 @@
 
 
 int main(int argc, char ** argv){
+
+   /*( if((atoi(argv[1]) < 1) || (atoi(argv[2]) < 1)){
+        printf("Number must be greater than 1\n");
+        return 0;
+    }
+*/
+
     printf("%d\n", Factorial(atoi(argv[1])));
     printf("%d\n", nCr(atoi(argv[1]), atoi(argv[2])));
     return 0;
