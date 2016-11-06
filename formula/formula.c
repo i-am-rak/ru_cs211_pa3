4ed1e21d45830a7a0e4b29da664a1713c6479a8f
raknahsa <raknahsa@gmail.com>
2016-11-06 14:06:01 -0500

    Add error handle
    Add code to handle errors such as negative and non-numeric answers. Also
    add an option for -h.

--- formula/formula.c
+++ formula/formula.c
@@ -2,17 +2,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+#include <string.h>
 
 int main(int argc, char ** argv){
-
-   /*( if((atoi(argv[1]) < 1) || (atoi(argv[2]) < 1)){
-        printf("Number must be greater than 1\n");
+    int x = atoi(argv[1]);    
+    if(argv[1][0] == '-' && argv[1][1] == 'h'){
+        fprintf(stdout,"Usage: formula <positive integer>\n");
         return 0;
     }
-*/
 
-    printf("%d\n", Factorial(atoi(argv[1])));
-    printf("%d\n", nCr(atoi(argv[1]), atoi(argv[2])));
+    int j = 0;
+    int isNotNum = 0;
+    for(j = 0; j < strlen(argv[1]); j++){
+        if(!isdigit(argv[1][j])){
+            isNotNum = 1;
+            break;
+        }
+    }
+
+    if((x < 0) || (isNotNum)){
+        fprintf(stderr,"ERROR: Expected non-negative integer\n");
+        return 0;
+    }
+    else if(x == 0){
+        fprintf(stdout,"(1+x)^0 = 1\n");
+        return 0;
+    }
+    else if(nCr(x,x/2) == 0){
+        fprintf(stderr,"ERROR: Integer overflow occured\n");
+        return 0;
+    }
+    else{
+        fprintf(stdout,"(1+x)^%s = 1 ", argv[1]);
+        int i = 0;
+        for(i = 1; i <= atoi(argv[1]); i++){
+            fprintf(stdout,"+ %d*x^%d", nCr(atoi(argv[1]),i), i);
+        }
+        fprintf(stdout,"\n");
+    }
     return 0;
 }
