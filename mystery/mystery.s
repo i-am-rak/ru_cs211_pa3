f0256de6190c6609209defb8418a965b8d224646
raknahsa <raknahsa@gmail.com>
2016-11-06 12:46:20 -0500

    Comment mystery.s and modify mystery2.c
    Comment each line of the mystery assembly code and write the equivalent
    c code in mystery2.c.

--- mystery/mystery.s
+++ mystery/mystery.s
@@ -21,53 +21,53 @@ dothething:
 	movl	num(,%eax,4), %eax  #move the the number at the array num at index from the integer
 	cmpl	$-1, %eax           #compare value at the array with negative one
 	je	.L4                     #if it is equal, jump to L4
-	movl	8(%ebp), %eax
-	movl	num(,%eax,4), %eax
-	movl	%eax, -24(%ebp)
+	movl	8(%ebp), %eax       #if its not equal move input to eax
+	movl	num(,%eax,4), %eax  #move the value at the array to eax
+	movl	%eax, -24(%ebp)     #move that value to offset -24 of ebp
 	jmp	.L6
 .L4:
 	movl	$-1, -8(%ebp)       #move negative one to ebp at -8 off
 	cmpl	$0, 8(%ebp)         #check if the integer entered in the funciton is 0
 	jne	.L7                     #jump if its not equal
-	movl	$0, -8(%ebp)        #if it is eaul, move 0 to the -8 position
+	movl	$0, -8(%ebp)        #if it is equal, move 0 to the -8 position
 	jmp	.L9                     #jump to L9
 .L7:
-	cmpl	$1, 8(%ebp)
-	jne	.L10
-	movl	$1, -8(%ebp)
-	jmp	.L9
+	cmpl	$1, 8(%ebp)         #check if the integer entered in the funciton is 1 
+	jne	.L10                    #jump to 10 if its not equal
+	movl	$1, -8(%ebp)        #jump move 1 to -8 offset if its equal
+	jmp	.L9                     #jump to L9
 .L10:
-	movl	8(%ebp), %eax
-	subl	$2, %eax
-	movl	%eax, (%esp)
-	call	dothething
-	movl	%eax, %ebx
-	movl	8(%ebp), %eax
-	subl	$1, %eax
-	movl	%eax, (%esp)
-	call	dothething
-	movl	%ebx, 4(%esp)
-	movl	%eax, (%esp)
-	call	add
-	movl	%eax, -8(%ebp)
+	movl	8(%ebp), %eax       #move the funciton input to eax
+	subl	$2, %eax            #subtract 2 from the eax and put in eax
+	movl	%eax, (%esp)        #move that value to esp
+	call	dothething          #call dothething with esp
+	movl	%eax, %ebx          #move the result of dothething to ebx
+	movl	8(%ebp), %eax       #move function input to eax
+	subl	$1, %eax            #subtract 1 from eax and put int eax
+	movl	%eax, (%esp)        #move that value to esp
+	call	dothething          #call do the thing on esp
+	movl	%ebx, 4(%esp)       #move ebx to 4 off esp
+	movl	%eax, (%esp)        #move eax to esp(at 0)
+	call	add                 #call add with esp(will use the 2 values in add function)
+	movl	%eax, -8(%ebp)      #move the value to -8 offset of ebp
 .L9:
 	movl	8(%ebp), %eax       #move the integer(input of function) to eax
 	movl	num(,%eax,4), %eax  #move the value of the array at index of that integer to eax
-	cmpl	$-1, %eax           #compare that value with negative 
-	jne	.L12
-	movl	8(%ebp), %edx
-	movl	-8(%ebp), %eax
-	movl	%eax, num(,%edx,4)
+	cmpl	$-1, %eax           #compare that value with negative  -1
+	jne	.L12                    #if its not equal, jump to 12
+	movl	8(%ebp), %edx       #if its equal move the input to gen %edx
+	movl	-8(%ebp), %eax      #move the value of offet -8(either 0/1 or the sum) to eax
+	movl	%eax, num(,%edx,4)  #move that to the value at index of the array
 .L12:
-	movl	8(%ebp), %eax
-	movl	num(,%eax,4), %eax
-	movl	%eax, -24(%ebp)
-.L6:
-	movl	-24(%ebp), %eax
-	addl	$28, %esp
-	popl	%ebx
-	popl	%ebp
-	ret
+	movl	8(%ebp), %eax       #move the integer of the function to eax
+	movl	num(,%eax,4), %eax  #move the value of the array at index of the integer to eax 
+	movl	%eax, -24(%ebp)     #move eax to -24 offset of ebp
+.L6:        
+	movl	-24(%ebp), %eax     #move the value at -24 to eax(value at the array) to be returned
+	addl	$28, %esp           #set stack pointer back
+	popl	%ebx                #pop ebx out of stack
+	popl	%ebp                #pop ebp out of stack
+	ret                         #return value of eax to main
 	.size	dothething, .-dothething
 	.section	.rodata
 .LC0:
@@ -76,40 +76,40 @@ dothething:
 .globl main
 	.type	main, @function
 main:
-	leal	4(%esp), %ecx
-	andl	$-16, %esp
-	pushl	-4(%ecx)
-	pushl	%ebp
-	movl	%esp, %ebp
-	pushl	%ecx
-	subl	$36, %esp
-	movl	4(%ecx), %eax
-	addl	$4, %eax
-	movl	(%eax), %eax
-	movl	%eax, (%esp)
-	call	atoi
-	movl	%eax, -12(%ebp)
-	movl	$0, -8(%ebp)
-	jmp	.L16
+	leal	4(%esp), %ecx       #move address 4 offset esp to gen % ecx
+	andl	$-16, %esp          #add -16 to esp
+	pushl	-4(%ecx)            #push value at -4 of ecx to stack
+	pushl	%ebp                #push ebp to stack
+	movl	%esp, %ebp          #move esp to ebp
+	pushl	%ecx                #push ecx to the stack
+	subl	$36, %esp           #subtract 36 from esp
+	movl	4(%ecx), %eax       #move 4 offset ecx to eax
+	addl	$4, %eax            #addthe value of 4 to eax
+	movl	(%eax), %eax        #move the adddress to address(above code could be done differently)
+	movl	%eax, (%esp)        #move eax to esp
+	call	atoi                #call atoi esp (convert the input to a integer
+	movl	%eax, -12(%ebp)     #move the value of input to -12 offset ebp
+	movl	$0, -8(%ebp)        #move value zero to -8 offset ebp
+	jmp	.L16                    #jump to L16
 .L17:
-	movl	-8(%ebp), %eax
-	movl	$-1, num(,%eax,4)
-	addl	$1, -8(%ebp)
+	movl	-8(%ebp), %eax      #move value at -8 offset ebp to eax
+	movl	$-1, num(,%eax,4)   #move the value -1 to the spot at array of index of -8 ebp
+	addl	$1, -8(%ebp)        #add -8 offset ebp by 1(increment by 1)
 .L16:
-	cmpl	$199, -8(%ebp)
-	jle	.L17
-	movl	-12(%ebp), %eax
-	movl	%eax, (%esp)
-	call	dothething
-	movl	%eax, 4(%esp)
-	movl	$.LC0, (%esp)
-	call	printf
-	movl	$0, %eax
-	addl	$36, %esp
-	popl	%ecx
-	popl	%ebp
-	leal	-4(%ecx), %esp
-	ret
+	cmpl	$199, -8(%ebp)      #compare 199 with the value at -8 offset ebp
+	jle	.L17                    #jump to L17 if less than or equal to 199
+	movl	-12(%ebp), %eax     #move the value of the input(-12(%ebp)) to eax
+	movl	%eax, (%esp)        #move that value to esp
+	call	dothething          #call dothething with esp
+	movl	%eax, 4(%esp)       #move the result to 4 offset esp
+	movl	$.LC0, (%esp)       #move the value at .LC0 to esp
+	call	printf              #call prinf witht esp
+	movl	$0, %eax            #move 0 to eax
+	addl	$36, %esp           #add 36 back to the esp
+	popl	%ecx                #pop ecx out of stack
+	popl	%ebp                #pop ebp out of stack
+	leal	-4(%ecx), %esp      #move address -4 offset to esp
+	ret                         #return to exit
 	.size	main, .-main
 	.comm	num,800,32
 	.ident	"GCC: (GNU) 4.1.2 20080704 (Red Hat 4.1.2-51)"
--- mystery/mystery2.c
+++ mystery/mystery2.c
@@ -8,21 +8,43 @@
 #include <string.h>
 #include <ctype.h>
 
+
+int num[200]; //Contains the first 200 numbers of fibonacci
+
 int add(int a1, int a2){
 
     return a1 + a2;
 }
 
 int dothething(int n){
+    int temp;
+    if(num[n] == -1){
+        if(n == 0){
+            temp = 0;
+        }
+        else if(n == 1){
+            temp = 1;
+        }
+        else{
+            temp = add(dothething(n-2),dothething(n-1));
+        }
+        if(num[n] == -1){
+            num[n] = temp;
+        }
+    }
     
-    
-
+    return num[n];
 }
 
 int main(int argc, char ** argv){
     
     int n = atoi(argv[1]);
     
+    int i = 0;
+    for(i = 0; i <= 199; i++){
+        num[i] = -1;
+    }
+
     printf("Value:  %d\n",dothething(n));
                     
     return 0;
